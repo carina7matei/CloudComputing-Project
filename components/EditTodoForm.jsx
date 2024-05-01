26ae767ba8c7b60ef553431c9aa5a175b58158d2
carina7matei <carinavaleriam@gmail.com>
2024-05-01 17:03:18 +0300

    rezolvare erori

--- components/EditTodoForm.jsx
+++ components/EditTodoForm.jsx
@@ -12,7 +12,7 @@ export default function EditTodoForm({id, title, description}){
     const handleSubmit= async (e)=>{
         e.preventDefault();
         try{
-            const res=await fetch(`http://localhost:3000/api/todos/${id}`,{
+            const res=await fetch(`/api/todos/${id}`,{
                 method:"PUT",
                 headers:{
                     "Content-type":"application/json"
