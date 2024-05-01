3f8da40ac3439819555e29f62fcd2ed7eaf6f561
carina7matei <carinavaleriam@gmail.com>
2024-05-01 15:14:54 +0300

    creare api pentru operatiile crud

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 
-const connectMongoDB=()=>{
+const  connectMongoDB=async()=>{
     try{
-    mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MONGODB_URI)
     console.log('connected to mongodb');
     }catch(err){
         console.log(err);
